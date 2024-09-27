dbfc1163b57c6bbaa868bba0f9b59c281cb094ec
Georjane <pzgeorjane@gmail.com>
2024-09-27 09:15:33 +0100

    Authentication models verified

---
+++ resources/views/donations/index.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        Hi Jane
+      
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DonationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Route::resource('donations', DonationController::class)
+//     ->only(['index', 'store'])
+//     ->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+    Route::post('campaigns', [CampaignController::class, 'create']);
+    Route::get('campaigns', [CampaignController::class, 'index']);
+    Route::post('campaigns/{campaign}/donate', [DonationController::class, 'donate']);
+    Route::get('donations', [DonationController::class, 'index']);
 });
 
 require __DIR__.'/auth.php';
